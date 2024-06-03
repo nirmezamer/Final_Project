6d1b1ac01520a2e581aad847935179d95bb5bc7e
nirmezamer <nirmezamer@mail.tau.ac.il>
2024-06-03 17:06:02 +0300

    initialize video_compressor.py

--- video_compressor.py
+++ video_compressor.py
@@ -1,10 +1,109 @@
-# TODO:
-# 1. try to read video file and split it into frames
-
 import matplotlib.pylab as plt
 import numpy as np
 import cv2
 import JPEG_compressor
+
+def find_the_most_similar_block(current_block, previous_frame, center, window_size=32):
+    """
+    :param      current_block: the block we try to compress using the most similar block in previous frame
+                previous_frame: np 2D array - last frame already compressed
+                window_size: limits the search borders
+                center: indices of the center block out of all 9 blocks, at first it will be the current_block location in P frame.
+    :return:    similar_block: the most similar block in previous frame to the current block
+                center: the motion vector for the most similar block
+    """
+    x, y = center
+    block_size = np.shape(current_block)[0]
+    margin = block_size // 2
+
+    if window_size < 1:
+        most_similar_block = previous_frame[x - margin: x + margin, y - margin: y + margin]
+        return most_similar_block, center
+
+    locations = []
+    rows, cols = np.shape(previous_frame)
+
+    if x + margin > rows:
+        x = rows - margin
+    if x < margin:
+        x = margin
+    if y + margin > rows:
+        y = cols - margin
+    if y < margin:
+        y = margin
+
+    # define the neighbors around current center
+    locations.append((x, y))
+    locations.append((min(x + window_size, rows - margin), y))
+    locations.append((max(x - window_size, margin), y))
+    locations.append((x, min(y + window_size, cols - margin)))
+    locations.append((x, max(y - window_size, margin)))
+    locations.append((min(x + window_size, rows - margin), max(y - window_size, margin)))
+    locations.append((min(x + window_size, rows - margin), min(y + window_size, cols - margin)))
+    locations.append((max(x - window_size, margin), max(y - window_size, margin)))
+    locations.append((max(x - window_size, margin), min(y + window_size, cols - margin)))
+
+    # chosen blocks to compare with the current block
+    blocks_to_compare = []
+    for location in locations:
+        blocks_to_compare.append(previous_frame[location[0] - margin: location[0] + margin,
+                                                location[1] - margin: location[1] + margin])
+
+    mses = []
+    # calculate all mse in relate to the current_block
+    for block in blocks_to_compare:
+        mse_value = np.mean(np.square(current_block - block))
+        mses.append(mse_value)
+
+    arg_min = np.argmin(mses)
+    curr_best_center = locations[arg_min]
+
+    # continue for the next iteration with the most similar block in the current neighborhood
+    return find_the_most_similar_block(current_block, previous_frame, curr_best_center, window_size//2)
+
+def compress_P_frame(I_frame, P_frame, block_size=8, window_size=32):
+    """
+    :param: I_frame: I_frame after YUV transform we use to compress the P_frame
+            P_frame: P_frame after YUV transform we want to compress in relate to the I_frame
+            block_size: the size of the block we want to compress
+            window_size: the size of the window we want to search for the most similar block
+
+    :return residuals_blocks: the residuals blocks
+            motion_vectors: the motion vectors
+    """
+    blocks_of_P_frame = JPEG_compressor.break_matrix_into_blocks(P_frame, block_size)
+    blocks_centers = get_block_centers(P_frame, block_size)
+
+    residuals_blocks = []
+    motion_vectors = []
+
+    for i, block in enumerate(blocks_of_P_frame):
+        most_similar_block, center = find_the_most_similar_block(block, I_frame, blocks_centers[i], window_size)
+        residuals_blocks.append(most_similar_block - block)
+        motion_vectors.append(center)
+
+    return residuals_blocks, motion_vectors
+
+def get_block_centers(frame, block_size=8):
+    """
+    :param frame:
+    :param block_size:
+    :return:
+    """
+    rows, cols = frame.shape
+    num_blocks_row = rows // block_size
+    num_blocks_col = cols // block_size
+
+    centers = []
+
+    # Calculate the centers of the blocks
+    for i in range(num_blocks_row):
+        for j in range(num_blocks_col):
+            center_row = i * block_size + block_size // 2
+            center_col = j * block_size + block_size // 2
+            centers.append((center_row, center_col))
+
+    return centers
 
 def read_video_file_and_break_into_frames(video_file_path):
     """
@@ -22,10 +121,6 @@ def read_video_file_and_break_into_frames(video_file_path):
         frames_list.append(frame)
     cap.release()
     return frames_list, frame_count
-
-def compress_P_frame(I_frame, P_frame):
-    # TODO: check with Guy what the prameters to send to the compress_P_frame function
-    pass
 
 def compress_video(video_file_path, I_frame_interval=10):
 
@@ -61,12 +156,11 @@ def compress_video(video_file_path, I_frame_interval=10):
             compress_P_frame(last_I_frame_Cb, P_frame_Cb)
             compress_P_frame(last_I_frame_Cr, P_frame_Cr)
 
-
 def main():
     video_file_path = 'videos_to_compress/earth_video.mp4'
     frames_list, frame_count = read_video_file_and_break_into_frames(video_file_path)
-    cv2.imshow('frame', frames_list[1300])
-    cv2.waitKey(5000)
+    import video_decompressor
+    video_decompressor.create_video_from_frames(frames_list, 'output_video.mp4')
     print('frame_count:', frame_count)
     return
 
--- video_decompressor.py
+++ video_decompressor.py
@@ -1,3 +1,26 @@
+import cv2
 
 def decompress_P_frame(I_frame, residuals_blocks, motion_vectors, block_size):
     pass
+
+
+def create_video_from_frames(frames_list, video_file_path):
+    """
+    Create video file from frames
+    :param frames_list:
+    :param video_file_path:
+    :return:
+    """
+    # take the frames_list and create a video file in mp4 format
+    height, width, layers = frames_list[0].shape
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(video_file_path, fourcc, 30, (width, height))
+    for frame in frames_list:
+        out.write(frame)
+    out.release()
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
